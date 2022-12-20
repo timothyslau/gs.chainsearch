d0c7652ee9b7c5fea2f5da2d91954a3252a8aa3f
Timothy Lau <laut@timothys-mbp-2.lan>
2022-12-20 14:54:22 -0700

    fixed the proxy list to allow for "https"

--- gs_scrape.R
+++ gs_scrape.R
@@ -24,26 +24,20 @@ url_list <- function(second_pg_url, from=0, to=900, by=20){
 url_list(second_pg_url = "https://scholar.google.com/scholar?start=20&hl=en&as_sdt=5,45&sciodt=0,45&as_yhi=2019&cites=10244671870114417611&scipsc=")
 
 
-# generate list of IP addresses for a redirect
-proxy_list <- function(limit=50) {
-  url <- httr::modify_url(
-    url = "https://proxylist.geonode.com/api/proxy-list", # url = "http://www.hidemyass.com/proxy-list",
-    query  = list(
-      limit = limit,
-      page = 1,
-      speed = "fast",
-      sort_by = "responseTime",
-      sort_type = "asc",
-      google = "true"
-    )
-  )
-  res <- httr::GET(url)
-  tmp1 <- httr::content(res)
-  tmp2 <- Reduce(rbind, tmp1$data)
-  return(data.frame(tmp2, row.names = NULL))
+# generate list of IP addresses for a proxy
+proxy_list <- function(https=TRUE, google=TRUE) {
+    https_flg <- ifelse(test = https == TRUE, yes = "yes", no = "no")
+    google_flg <- ifelse(test = google == TRUE, yes = "yes", no = "no")
+    url <- "https://free-proxy-list.net"
+    html <- rvest::read_html(x = url)
+    element <- rvest::html_element(x = html, xpath = '//*[@id="list"]/div/div[2]/div/table')
+    table <- rvest::html_table(element)[,c(1,2,4,6,7,8)]
+    colnames(table) <- c("ip", "port", "country", "google", "https", "last_checked")
+    out <- subset(x = table, subset = (google == google_flg) & (https == https_flg))
+    return(out)
 }
 
-proxy_list()[,c("ip", "port")]
+proxy_list()
 
 
 # generate list of user agents
@@ -56,52 +50,85 @@ ua_list <- function(){
   return(table)
 }
 
-ua_list()[,"ua"]
+ua_list()
+
+
+
+# TEST - ((PASSING))
+unit_test <- function(test="ua", proxy_ip, proxy_port, my_ua){
+  if (test == "ua") {
+    url <- "http://httpbin.org/user-agent"
+    }
+  else if (test == "ip") {
+    url <- "https://httpbin.org/ip"
+  }
+  
+  out <- httr::GET(
+    url = url, 
+    httr::user_agent(agent = my_ua),
+    httr::use_proxy(url = proxy_ip, port = proxy_port),
+    verbose()
+  )
+  
+  return(cat(content(x = out, as = "text", encoding = "UTF-8")))
+}
 
 my_ua <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15"
 
-# verified that UA is being changed
-rvest::session(url = "https://httpbin.org/user-agent", httr::user_agent(my_ua))$response$request$options$useragent
+unit_test(
+  test = "ua",
+  my_ua = my_ua,
+  proxy_ip = "190.61.88.147",
+  proxy_port = 8080)
+
+unit_test(
+  test = "ip",
+  my_ua = my_ua,
+  proxy_ip = "190.61.88.147",
+  proxy_port = 8080)
 
 
 
 
+# SCRAPE WEBPAGES ---------------------------------------------------------
 
-httr::set_config(config = httr::use_proxy(url = "url", port = "port"))
+test_url <- url_list(second_pg_url = "https://scholar.google.com/scholar?start=20&hl=en&as_sdt=5,45&sciodt=0,45&as_yhi=2019&cites=10244671870114417611&scipsc=")[2]
+test_proxy_ip <- proxy_list(https = T, google = T)$ip[2]
+test_proxy_port <- proxy_list(https = T, google = T)$port[2]
+test_ua <- ua_list()$ua[1]
 
-
-# save webpages
-save_html <- function(url, pause = 0.5, backoff = FALSE){
-  t0 <- Sys.time()
-  pause <- pause * stats::runif(n = 1, min = 0.5, max = 1.5)
+save_wp <- function(dir=NA, encoding = "UTF-8", url, ip, port, ua){
+  if (is.na(dir)) {dir <- getwd()}
   
-  #initiate scrape and detect redirect
-  html <- RCurl::getURL(url, followlocation=TRUE, .opts=list(useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15", "Cache-Control"="no-cache"))
-  names(html) <- url
+  wp <- httr::GET(
+    url = url,
+    httr::user_agent(agent = ua),
+    httr::use_proxy(url = ip, port = port),
+    httr::verbose()
+    )
   
-  t1 <- Sys.time()
-  response_delay <- round(as.numeric(t1-t0), 3)
-  if(backoff == TRUE){
-    #  message(paste('Saved in',
-    #                round(response_delay, 3),
-    #              'seconds. Waiting',
-    #              round(pause*response_delay, 3),
-    #              'seconds...'))
-    Sys.sleep(pause*response_delay)
-  } else {
-    #  message(paste('Saved in',
-    #                round(response_delay, 3),
-    #              'second. Waiting',
-    #              round(pause, 3),
-    #             'seconds...'))
-    Sys.sleep(pause)
+  if (wp$status_code == 200) {
+    file_nm <- paste0(unlist(strsplit(x = unlist(strsplit(x = url, split = "cites="))[2], split = "&scipsc=")), "-", as.numeric(unlist(regmatches(x = url, m = regexpr(pattern = "[[:digit:]]+", text = url)))), ".html")
+    cat(httr::content(x = wp, as = "text", encoding = encoding), file = paste0(dir, "/", file_nm))  
   }
-  
-  return(html)
+  else {
+    message("Page Load Failure: Build a better function for handling this!")
+  }
   
 }
 
-# SCRAPE WEBPAGE ----------------------------------------------------------
+save_wp(dir = "/Users/laut/Downloads", url = test_url, ip = test_proxy_ip, port = test_proxy_port, ua = test_ua)
+
+
+
+
+# PROCESS WEBPAGES --------------------------------------------------------
+
+unlist(mapply(FUN = function(x, times){rep(x = x, times = times)}, x = 1:4, times = 4:1, SIMPLIFY = TRUE))
+rep(x = 1:4, times = 4:1)
+
+
+
 
 
 # NOTES -------------------------------------------------------------------
